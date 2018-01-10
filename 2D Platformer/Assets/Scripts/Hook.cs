da82f1f6bd839a50c93760ba5bd3ded176dea3d5
antonym0us <hak1@williams.edu>
2018-01-10 18:54:07 -0500

    day 1 hook

---
+++ 2D Platformer/Assets/Scripts/Hook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 
+public class Hook : MonoBehaviour {
+	public Transform parentTransform;
+	private RaycastHit hit;
+	public Rigidbody rb;
+	public bool hooked = false;
+	private float momentum;
+	public float speed;
+	private float step;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var v3 = Input.mousePosition;
+		v3.z = 0;
+		v3 = Camera.main.ScreenToWorldPoint (v3);
+		v3.z = 0;
+		this.transform.LookAt (v3);
+		if (Input.GetButtonDown ("Fire1")) {
+			if(Physics.Raycast(this.transform.position, this.transform.rotation * Vector3.forward, out hit)) {
+				hooked = true;
+				rb.isKinematic = true;
+			}				
+		}
+		if (Input.GetButtonUp ("Fire1")) {
+			if (hooked) {
+				hooked = false;
+				rb.isKinematic = false;
+				rb.velocity = this.transform.rotation * Vector3.forward * momentum;
+			}
+		}
+		if (hooked) {
+			momentum += Time.deltaTime * speed;
+			step = momentum * Time.deltaTime;
+			parentTransform.position = Vector3.MoveTowards (parentTransform.position, hit.point, step);
+		} else if (momentum >= 0) {
+			momentum -= Time.deltaTime * 5;
+			step = 0;
+		}
+	}
+
+			
+	void FixedUpdate() {
+		//if (!hooked) {
+		//}
+	}
+}
