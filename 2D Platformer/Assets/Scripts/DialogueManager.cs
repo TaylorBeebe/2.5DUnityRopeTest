9829443de441bced822d43cd5d75eb965d15eeaf
tdb1 <taylor.d.beebe@gmail.com>
2018-01-11 17:22:05 -0500

    Health bar and tool bar updated. Dialogue box implemented.

---
+++ 2D Platformer/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour {
+
+	private Queue<string> sentences;
+
+	public Text nameText;
+	public Text dialogueText;
+
+	void Start () {
+		sentences = new Queue<string> ();
+	}
+
+	public void StartDialogue (Dialogue d){
+
+		Debug.Log ("starting conversation");
+
+		sentences.Clear ();
+		nameText.text = d.name;
+		foreach (string sentence in d.sentences) {
+			sentences.Enqueue (sentence);
+		}
+		DisplayNextSentence ();
+	}
+
+	public void DisplayNextSentence(){
+		if (sentences.Count == 0) {
+			EndDialogue ();
+			return;
+		}
+		string sentence = sentences.Dequeue ();
+		dialogueText.text = sentence;
+	}
+
+	void EndDialogue(){
+		Debug.Log ("Ending dialogue");
+	}
+}
---
+++ 2D Platformer/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public Dialogue d;
+
+	public void TriggerDialog(){
+		FindObjectOfType<DialogueManager> ().StartDialogue (d);
+	}
+}
