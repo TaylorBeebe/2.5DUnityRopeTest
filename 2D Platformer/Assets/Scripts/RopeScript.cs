2f89dbdec88d416596489d0c7a2833c0f2e6051d
antonym0us <hak1@williams.edu>
2018-01-19 19:08:07 -0500

    day 3 hook: rudimentary RopeScript (debugging needed)

--- 2D Platformer/Assets/Scripts/RopeScript.cs
+++ 2D Platformer/Assets/Scripts/RopeScript.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class RopeScript : MonoBehaviour {
 
+	public Vector3 destiny;
+	public float speed = 1;
+	public float distance = 2;
+	public GameObject nodePrefab;
+	public GameObject player;
+	public GameObject lastNode;
+	private bool done = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.FindGameObjectWithTag ("Player");
+		lastNode = transform.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = Vector3.MoveTowards (transform.position, destiny, speed);
+
+		if ((Vector3)transform.position != destiny) {
+			if (Vector3.Distance (player.transform.position, lastNode.transform.position) > distance) {
+				CreateNode ();
+			}
+		} else if (done == false) {
+			done = true;
+			lastNode.GetComponent<HingeJoint> ().connectedBody = player.GetComponent<Rigidbody> ();
+		}
+	}
+	void CreateNode() {
+		Vector3 pos2Create = player.transform.position - lastNode.transform.position;
+		pos2Create.Normalize ();
+		pos2Create *= distance;
+		pos2Create += (Vector3)lastNode.transform.position;
+
+		GameObject go = (GameObject)Instantiate (nodePrefab, pos2Create, Quaternion.identity);
+
+		go.transform.SetParent (transform);
+
+		lastNode.GetComponent<HingeJoint> ().connectedBody = go.GetComponent<Rigidbody> ();
+		lastNode = go;
 	}
 }
