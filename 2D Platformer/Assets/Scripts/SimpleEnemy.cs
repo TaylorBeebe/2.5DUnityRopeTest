0563573d6ae3b511eb715a66cb2bc9fa657acf69
antonym0us <hak1@williams.edu>
2018-02-27 17:09:38 -0500

    layout v1
    simple enemy behavior
    platform behavior (wip)

---
+++ 2D Platformer/Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour {
+	public Transform Player;
+	int moveSpeed = 3;
+	int maxDist = 100;
+	int minDist = 5;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (Player);
+		if (Vector3.Distance (transform.position, Player.position) >= minDist) {
+			transform.position = Vector3.MoveTowards(this.transform.position, Player, moveSpeed * Time.deltaTime);
+
+			if (Vector3.Distance (transform.position, Player.position) <= maxDist) {
+				
+			}
+		}
+	}
+}
---
+++ 2D Platformer/Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+	public Transform teleporterCounterpart;
+	public Transform player;
+	int distanceThreshold = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, player.position) >= distanceThreshold) {
+			if (Input.GetButtonDown (KeyCode.W)) {
+				player.position = teleporter1.position;
+			}
+		}
+	}
+}
