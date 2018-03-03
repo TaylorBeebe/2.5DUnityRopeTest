e9e8e7f4cea520160365638b1713644c9721493e
tdb1 <taylor.d.beebe@gmail.com>
2018-03-03 14:12:18 -0500

    Last rope implementation was too buggy. Now implementing it a different way. Foundation in place for new implementation.

--- 2D Platformer/Assets/Scripts/RopeScript.cs
+++ 2D Platformer/Assets/Scripts/RopeScript.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class RopeScript : MonoBehaviour {
 
+	//Boolean checks if the hook has hit an object or has been interupted
+	private bool extending = true;
+
+	private bool hooked = false;
+
+	//Boolean checks if hook is currently retracting due to interference
+	private bool interferenceRetracting = false;
+
+	private bool hookLatched = false;
+
+	//Ray used to make sure nothing has come between hook and origin
+	private Ray ray;
+
 	//Speed the hook retracts
-	public float retractSpeed = 2;
+	public float retractSpeed = 2f;
+
+	//Speed hook retracts if interference detected
+	public float interferenceRetractSpeed = 5f;
 
 	//furthest the rope can go
-	public float maxDistance = 50;
+	public float maxDistance = 50f;
 
 	//distance constant that sets how far away the last node is before
 	//a new node is created
@@ -23,7 +39,7 @@ public class RopeScript : MonoBehaviour {
 
 	//stores a list of all nodes currently in the rope
 	//private List<GameObject> nodes = new List<GameObject> ();
-	private Stack<GameObject> nodes = new Stack<GameObject>();
+	private List<GameObject> nodes = new List<GameObject>();
 
 	//renders line between each rope node
 	private LineRenderer line;
@@ -46,31 +62,37 @@ public class RopeScript : MonoBehaviour {
 
 
 	void Start(){
+		//Gets the line render component
 		line = this.GetComponent<LineRenderer> ();
-		lastNode = this.gameObject;
-		nodes.Push (this.gameObject);
+
+		//Get reference to Player
 		player = GameObject.FindGameObjectWithTag ("Player");
-		this.gameObject.GetComponent<Rigidbody> ().useGravity = false;
+
+		//Get the hook's origin
 		hookOrigin = GameObject.FindGameObjectWithTag ("HookOrigin");
-		Debug.Log (hookOrigin);
 	}
 
 	void Update(){
-
-		nodeSpawn = hookOrigin.transform.rotation * Vector3.forward * .02f;
-
-		if (Vector2.Distance (this.transform.position, hookOrigin.transform.position) 
-			<= maxDistance && !pullRopeIn) {
-
-			extendRope ();
 		
-		} else {
-			
-			retractRope ();
-		
-		}	
+		//Check if rope was interfered with during deployment, destroy if reached origin 
+		if (interferenceRetracting) {
+			if (Vector3.Distance (this.transform.position, hookOrigin.transform.position) <= 0.2f) {
+				Destroy (this);
+			}
+		} 
+
+		else if (extending) {
+			if (Physics.Linecast (transform.position, hookOrigin.transform.position, 8) ||
+				Vector3.Distance(this.transform.position, hookOrigin.transform.position) >= maxDistance) {
+				extending = false;
+				interferenceRetracting = true;
+				this.GetComponent<Collider> ().enabled = false;
+				Vector3.MoveTowards (transform.position, hookOrigin.transform.position, interferenceRetractSpeed);
+			}
+		}
+
+
 	}
-
 	void LateUpdate(){
 
 //		for (int x = 0; x < nodes.Count; x++) {
@@ -80,41 +102,20 @@ public class RopeScript : MonoBehaviour {
 	}
 	void extendRope(){
 		
-		if (Vector2.Distance (lastNode.transform.position, nodeSpawn) >= minDistance) {
-			newNode = (GameObject)Instantiate (ropeNode,
-				nodeSpawn,
-				Quaternion.identity
-			);
-			lastNode.GetComponent<ConfigurableJoint> ().connectedBody = newNode.GetComponent<Rigidbody> ();
-			newNode.GetComponent<ConfigurableJoint> ().connectedBody = hookOrigin.GetComponent<Rigidbody> ();
-			lastNode = newNode;
-		} else {
-			player.GetComponent<HookScript> ().extending = false;
-			pullRopeIn = true;
-			this.gameObject.GetComponent<Rigidbody> ().useGravity = true;
-		}
 	
 	}
 
 	void retractRope(){
-		
-		if (Vector2.Distance (lastNode.transform.position, hookOrigin.transform.position) <= minDistance) {
-			if (lastNode == this.gameObject) {
-				player.GetComponent<HookScript> ().extending = false;
-			}
 
-			Destroy (nodes.Pop ());
-			lastNode = nodes.Peek ();
-			Debug.Log ("Node has been destroyed. Last node is: " + lastNode);
-			lastNode.GetComponent<ConfigurableJoint> ().connectedBody = hookOrigin.GetComponent<Rigidbody>();
-		}
-		lastNode.GetComponent<Rigidbody> ().AddForce (hookOrigin.transform.position
-		- lastNode.transform.position * retractSpeed);
+
 	}
 
 	void OnCollisionEnter(Collision col){
 
 		this.gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+
+		hooked = true;
 		
 	}
+
 }
