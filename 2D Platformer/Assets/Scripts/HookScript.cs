0446a74b5d81f2f00452d20ecaf53f934e46f3c3
tdb1 <taylor.d.beebe@gmail.com>
2018-03-10 00:27:46 -0500

    HookScript and Ropescript Merged
    Hookscript and Ropescript have been merged to make the rest of the rope implementation easier. This took surprisingly long, more optimizations added. Still a ways to go.

--- 2D Platformer/Assets/Scripts/HookScript.cs
+++ 2D Platformer/Assets/Scripts/HookScript.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class HookScript : MonoBehaviour {
 
+
+
 	//Max length hook can travel
 	public float maxRopeLength = 30f;
+
+
+
+	//Stores destination of hook
+	private Vector3 hookDestination;
+
+
+
+	//List contating all the nodes in the rope
+	private Stack<GameObject> nodes = new Stack<GameObject> ();
+
+	//stores current speed of hook
+	private float curHookSpeed;
+
+	//Speed the hook retracts
+	public float retractSpeed = 100f;
+
+	//Speed the hook extends
+	public float extendSpeed = 35f;
+
+
+
+	//Boolean holds if the hook has hit an object or has been interupted
+	public bool extending;
+
+	//Boolean holds if the hook is currently retracting
+	public bool retracting = false;
 
 	//True if hook is currently deployed
 	private bool hookExtended = false;
@@ -16,8 +45,15 @@ public class HookScript : MonoBehaviour {
 	//True if player is currently hooked
 	public bool hooked = false;
 
+
+
 	//Main animator
 	Animator animator;
+
+
+
+	//Last node in the rope (Nodes are set when player is out of sight of hook)
+	private GameObject lastNode;
 
 	//cube located on player hook arm
 	public GameObject hookOrigin;
@@ -28,30 +64,54 @@ public class HookScript : MonoBehaviour {
 	//Instance of hook
 	public GameObject hookShot;
 
-	//Direction hook will be fired
-	private Vector3 shootDirection;
-
 	//Cube that follows mouse to test for mouse location on screen
 	public GameObject reticuleTestCube;
 
-	//Renders line when player is holding down hook fire button
-	public LineRenderer lineRenderer;
 
-	//Stores destination of hook
-	private Vector3 hookDestination;
+
+	//Color of linerenderer when aiming hook
+	public Material lineRendererFiringColor;
+
+	//Color of linerender when rope is deployed
+	public Material lineRendererRopeColor;
+
+
+
+	//Direction hook will be fired
+	private Vector3 shootDirection;
+
+
+
+	//Renders line when player is holding down hook fire button
+	private LineRenderer lineRenderer;
+
+
+
+	//Ray used to make sure nothing has come between hook and origin
+	private Ray ray;
 
 	//Raycast for finding destination of hook
 	private RaycastHit hit;
 
+
+
 	// Initialize Line, Animator
 	void Start () {
+		
 		animator = this.GetComponent<Animator> ();
+
 		lineRenderer = this.GetComponent<LineRenderer> ();
 		//rb = this.GetComponent<Rigidbody> ();
+
+		//Push the origin of hook onto stack
+		nodes.Push (hookOrigin);
+
+		extending = false;
+
 	}
 
 	void Update (){
-
+		
 		shootDirection = Input.mousePosition;
 		shootDirection.z = 5f; // Camera is at z = -5
 		shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
@@ -70,14 +130,19 @@ public class HookScript : MonoBehaviour {
 			
 //			Debug.Log ("Setting retracting to true");
 
-			hookShot.GetComponent<RopeScript> ().retracting = true;
 			hooked = false;
+			extending = false;
+			retracting = true;
+
+			StopCoroutine ("HookMovement");
+			StartCoroutine ("HookMovement");
+
 		} 
 
 		else if (Input.GetKeyDown("e") && hooked) {
 		}
 			
-		if (Input.GetButtonUp ("Fire1") && !hookExtended) {
+		if (Input.GetButtonUp ("Fire1") && !hookExtended && firing) {
 			//Stop the firing loop from running
 			firing = false;
 			//Reset Vertices in Line Renderer
@@ -92,10 +157,24 @@ public class HookScript : MonoBehaviour {
 			Invoke("WaitToShoot", 0.1f);
 		}
 
-		//If hook has been destroyed, set hookExtended to false
-		else if (hookShot == null) {
-			hookExtended = false;
-		}
+		//Check if rope was interfered with during deployment, destroy if reached origin 
+		if (extending) {
+			
+			if (Physics.Linecast (hookShot.transform.position, hookOrigin.transform.position)) {
+
+				Debug.Log ("Extension interupted!");
+
+				extending = false;
+				retracting = true;
+				hookDestination = nodes.Peek ().transform.position;
+
+				hookShot.GetComponent<Collider> ().enabled = false;
+
+				StopCoroutine ("HookMovement");
+				StartCoroutine ("HookMovement");
+
+			}
+		} 
 	}
 
 	void LateUpdate(){
@@ -106,14 +185,26 @@ public class HookScript : MonoBehaviour {
 			} else {
 				hookDestination = hookOrigin.transform.position + hookOrigin.transform.forward * maxRopeLength;
 			}
-
 			//Always set z = 0
 			hookDestination.z = 0f;
+
+			lineRenderer.material = lineRendererFiringColor;
 
 			//Set the vertices of Line Renderer to display where the hook will land
 			lineRenderer.SetPosition (0, hookOrigin.transform.position);
 			lineRenderer.SetPosition (1, hookDestination);
-		}
+		} else if (extending || retracting || hooked) {
+			
+
+			lineRenderer.material = lineRendererRopeColor;
+
+			lineRenderer.SetPosition (0, hookOrigin.transform.position);
+			lineRenderer.SetPosition (1, hookShot.transform.position);
+
+		} else {
+			lineRenderer.SetPosition (0, Vector3.zero);
+			lineRenderer.SetPosition (1, Vector3.zero);
+		} 
 	}
 
 	Vector3 cubeTransformPosition(){
@@ -134,8 +225,9 @@ public class HookScript : MonoBehaviour {
 
 		Vector3 hookSpawn = hookOrigin.transform.position + hookOrigin.transform.forward * 0.09f;
 
-		//Debug.DrawRay (hook.transform.position, hook.transform.forward * maxRopeLength, Color.green, 2f);
+		//Debug.DrawRay (hookSpawn, hookOrigin.transform.forward * maxRopeLength, Color.green, 2f);
 
+		//Calculate where hook will hit
 		if (Physics.Raycast (hookSpawn, hookOrigin.transform.forward * maxRopeLength, out hit, maxRopeLength)) {
 			hookDestination = hit.point;
 		} else {
@@ -144,8 +236,82 @@ public class HookScript : MonoBehaviour {
 
 		//Always align hook destination with z = 0
 		hookDestination.z = 0f; 
-		Debug.Log ("Destination at instantiation: " + hookDestination);
+//		Debug.Log ("Destination at instantiation: " + hookDestination);
+
 		hookShot = (GameObject) Instantiate (hook, hookSpawn, hookOrigin.transform.rotation);
-		hookShot.GetComponent<RopeScript> ().destination = hookDestination;
+		extending = true;
+		StartCoroutine ("HookMovement");
+
+		//hookShot.GetComponent<RopeScript> ().destination = hookDestination;
 	}
+
+	IEnumerator HookMovement(){
+		//Excutes a loop until hook as reached its target
+
+		Debug.Log ("Extending initially set to true");
+		while (true) {
+//			Debug.Log ((Vector3.Distance (this.transform.position, destination) <= 0.05f));
+//			Debug.Log (Vector3.Distance (this.transform.position, destination));
+//			Debug.Log ("Within Coroutine");
+
+			if (hooked || hookShot == null) {
+				yield break;
+			}
+			//Checks the distance between hook and destionation
+			if (Vector3.Distance (hookShot.transform.position, hookDestination) <= 0.01f){
+				
+				Debug.Log ("Distance between this and destination is less than 0.01f: " + hookDestination);
+
+				//If the hook is currently retracting, check if there is another node in the sequence to go to
+				if (retracting) {
+
+//					Debug.Log ("Hook is retracting");
+
+					curHookSpeed = retractSpeed;
+					if (nodes.Count > 1) {
+						nodes.Pop ();
+						hookDestination = nodes.Peek ().transform.position;
+					} else {
+						retracting = false;
+						hookExtended = false;
+//						lineRenderer.SetPosition (0, Vector3.zero);
+//						lineRenderer.SetPosition (1, Vector3.zero);
+ 						Destroy (hookShot);
+						yield break;
+					}
+
+					//If not retracting, this must be the final destination
+				} else if (!hooked) {
+
+//					Debug.Log ("Reached limit and not hooked. Retracting");
+
+					extending = false;
+					retracting = true;
+					hookShot.GetComponent<Collider> ().enabled = false;
+					hookDestination = nodes.Peek ().transform.position;
+					yield return null;
+				}
+			} else {
+
+//				Debug.Log ("In else statement of coroutine");
+//				Debug.Log (Vector3.Distance (hookShot.transform.position, hookDestination));
+
+				if (retracting) {
+					Debug.Log ("Retracting = true");
+					curHookSpeed = retractSpeed;
+					hookDestination = nodes.Peek ().transform.position;
+
+				} else{
+					curHookSpeed = extendSpeed;
+				}
+				hookShot.transform.position = Vector3.MoveTowards (hookShot.transform.position, hookDestination, extendSpeed * Time.deltaTime);
+
+//				Debug.Log ("Yielding return null");
+
+				yield return null;
+			}
+		}
+
+	}
+
 }
--- 2D Platformer/Assets/Scripts/RopeScript.cs
+++ 2D Platformer/Assets/Scripts/RopeScript.cs
@@ -4,187 +4,26 @@ using UnityEngine;
 
 public class RopeScript : MonoBehaviour {
 
-	//Boolean holds if the hook has hit an object or has been interupted
-	private bool extending = true;
-
-	//Boolean holds if the hook successfully attached to an object
-	private bool hooked = false;
-
-	//Boolean holds if the hook is currently retracting
-	public bool retracting = false;
-
-	//Boolean holds if hook has reached last node during retraction
-	private bool arrivedToLastRetractionNode;
-
-	//Ray used to make sure nothing has come between hook and origin
-	private Ray ray;
-
-	//Speed the hook retracts
-	public float retractSpeed = 100f;
-
-	//Speed the hook extends
-	public float extendSpeed = 35f;
-
-	//furthest the rope can go
-	public float maxDistance = 50f;
-
-	//stores current speed of hook
-	private float curHookSpeed;
-
-	//Where hook is heading
-	public Vector3 destination;
-
-	//Last node in the rope (Nodes are set when player is out of sight of hook)
-	public GameObject lastNode;
-
-	//distance constant that sets how far away the last node is before
-	//a new node is created
-	public float minDistance = 0.3f;
-
-	//renders line between each rope node
-	private LineRenderer line;
-
-	//origin of the hook on players arm
-	private GameObject hookOrigin;
-
 	//player gameobject
 	private GameObject player;
 
-	//List contating all the nodes in the rope
-	private Stack<GameObject> nodes = new Stack<GameObject> ();
-
-
 	void Start(){
-		//Gets the line render component
-		line = this.GetComponent<LineRenderer> ();
-
+		
 		//Get reference to Player
 		player = GameObject.FindGameObjectWithTag ("Player");
 
-		//Get the hook's origin
-		hookOrigin = GameObject.FindGameObjectWithTag ("HookOrigin");
-
-		//Push the origin of hook onto stack
-		nodes.Push (hookOrigin);
-
-		//last node is intially the hoook
-		lastNode = this.gameObject;
-
-		//Hook is initially extending
-		extending = true;
-
-		retracting = false;
-
-		//Start coroutine that controls hook movement
-		StartCoroutine ("HookMovement");
-
-	}
-
-	void Update(){
-
-		//Check if rope was interfered with during deployment, destroy if reached origin 
-		if (extending) {
-			if (Physics.Linecast (transform.position, hookOrigin.transform.position)) {
-
-				Debug.Log ("Extension interupted!");
-				extending = false;
-				retracting = true;
-				destination = nodes.Peek ().transform.position;
-
-				this.GetComponent<Collider> ().enabled = false;
-
-				StopCoroutine ("HookMovement");
-				StartCoroutine ("HookMovement");
-			
-			}
-		} 
-
-//		else if (hooked) {
-//
-//		}
-
-	}
-	void LateUpdate(){
-
-		line.SetPosition (0, hookOrigin.transform.position);
-		line.SetPosition (1, this.transform.position);
-		
 	}
 
 	void OnCollisionEnter(Collision col){
 		Debug.Log ("On collision enter");
+		Debug.Log (col.gameObject.tag);
 		if (col.gameObject.tag == "Player" || col.gameObject.tag == "HookOrigin") {
 			Physics.IgnoreCollision (col.collider, this.GetComponent<Collider> ());
 		} else {
 			Debug.Log ("COLLIDED");
-			extending = false;
-			hooked = true;
 
 			player.GetComponent<HookScript> ().hooked = true;
+			player.GetComponent<HookScript> ().extending = false;
 		}
-	}
-
-	IEnumerator HookMovement(){
-		//Excutes a loop until hook as reached its target
-		while (true) {
-//			Debug.Log ((Vector3.Distance (this.transform.position, destination) <= 0.05f));
-//			Debug.Log (Vector3.Distance (this.transform.position, destination));
-
-//			Debug.Log ("Within Coroutine");
-//			Debug.Log ("Hooked: " + hooked);
-//			Debug.Log ("Retracting: " + retracting);
-
-			if (hooked) {
-				yield break;
-			}
-
-			//Checks the distance between hook and destionation
-			if (Vector3.Distance (this.transform.position, destination) <= 0.01f){
-				
-//				Debug.Log ("Distance between this and destination is less than 0.05f");
-
-				//If the hook is currently retracting, check if there is another node in the sequence to go to
-				if (retracting) {
-					
-//					Debug.Log ("Hook is retracting");
-
-					curHookSpeed = retractSpeed;
-					if (nodes.Count > 1) {
-						nodes.Pop ();
-						destination = nodes.Peek ().transform.position;
-					} else {
-						Destroy (gameObject);
-						yield break;
-					}
-
-				//If not retracting, this must be the final destination
-				} else if (!hooked) {
-					
-//					Debug.Log ("Reached limit and not hooked. Retracting");
-
-					extending = false;
-					retracting = true;
-					destination = nodes.Peek ().transform.position;
-					yield return null;
-				}
-			} else {
-				
-//				Debug.Log ("In else statement of coroutine");
-
-				if (retracting) {
-					curHookSpeed = retractSpeed;
-					destination = nodes.Peek ().transform.position;
-					
-				} else{
-					curHookSpeed = extendSpeed;
-				}
-				transform.position = Vector3.MoveTowards (transform.position, destination, extendSpeed * Time.deltaTime);
-
-//				Debug.Log ("Yielding return null");
-
-				yield return null;
-			}
-		}
-			
 	}
 }
